7e2e663f5cf4fcc23588486795784d3b231af179
Sameer Tharakan <sameer@ronaldo.ices.utexas.edu>
2014-10-05 15:34:47 -0500

    updated loaddata.m to be able to load some new files

--- matlab/loaddata.m
+++ matlab/loaddata.m
@@ -1,4 +1,4 @@
-function [Xtrain,Ytrain,Xtest,Yexact,n] = loaddata(dataset,labeled)
+function [Xtrain,Ytrain,Xtest,Yexact,n] = loaddata(dataset,dir,labeled)
 %LOADDATA will output the training and testdata set given which file to load
 % under the following specifications
 % 'spiral' - test/trainspiral_cs8000_n05
@@ -8,11 +8,12 @@ if ~exist('labeled') %ignored for non-synthetic datasets
 				labeled = 0;
 end
 
+addpath(dir);
 
 if isequal(dataset, 'spiral')
 				filename = 'spiral_cs8000_n05';
 				disp('Loading training data ... ')
-				load(['train',filename]);
+				load([dir,'train',filename]);
 				[n,~] = size(X); n = n/2;
 
 				if ~exist('labeled') %ignored for non-synthetic datasets
@@ -26,25 +27,70 @@ if isequal(dataset, 'spiral')
 				Ytrain = zeros(size(Y));
 				Ytrain(1:2*labeled) = Y(idx(1:2*labeled));
 				disp('Loading test data ... ')
-				load(['test',filename]);
+				load([dir,'test',filename]);
 				Xtest = X;
 				Yexact = Y;
 				disp('Done loading data!')
 
 elseif isequal(dataset,'wine')
+				
 				disp('Reading in red wine data')
-				data_red = dlmread('/scratch/data/machine_learning/winequality-red.csv',';',1,0);
+				filename = 'winequality-';
+				data_red = dlmread([dir,filename,'red.csv'],';',1,0);
 
 				disp('Reading in white wine data')
-				data_white = dlmread('/scratch/data/machine_learning/winequality-white.csv',';',1,0);
+				data_white = dlmread([dir,filename,'white.csv'],';',1,0);
 
 				Xtrain = [data_red(:,1:end-1) ; data_white(:,1:end-1)];
-				Xtest = Xtrain;
+				Xtest = [];
 				Ytrain = [data_red(:,end); data_white(:,end)];
-				Yexact = Ytrain;
-				disp('Done loading data!')
+				Yexact = [];
 				[n, ~] = size(Xtrain);
-else 
+				disp('Done loading data! (Test and training are the same)')
+
+elseif isequal(dataset,'susy')
+				
+				disp('Reading in SUSY data')
+				filename = 'SUSY.csv';
+				x = load([dir,filename]);
+				
+				Ytrain = x(:,1);
+				Yexact = [];
+				Xtrain = x(:,2:end);
+				Xtest = [];
+				[n,~] = size(Xtrain);
+				disp('Done loading data! (Test and training are the same)')
+
+elseif isequal(dataset,'cpu')
+
+				disp('Reading in cpusmall data');
+				filename = 'cpusmall';
+				fid = fopen([dir,filename]);
+				x = textscan(fid,'%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f %d:%f');
+				fclose(fid);
+				idx = 1 + 2.*(1:12);
+				
+				Ytrain = x{1}; Yexact = [];
+				Xtrain = zeros(length(x{1}),12);
+				Xtest = [];
+				for i = 1:length(idx)
+								Xtrain(:,i) = x{idx(i)};
+				end
+				n = length(x{1});
+				disp('Done loading data! (Test and training are the same)');
+
+elseif isequal(dataset,'covtype')
+				
+				disp('Reading in covertype data')
+				filename = 'covtype.data';
+				x = csvread(filename);
+				Xtrain = x(:,1:end-1);
+				[n,~] = size(Xtrain);
+				Ytrain = x(:,end);
+				Yexact = []; Xtest = [];
+				disp('Done loading data! (Test and training are the same)');
+
+else
 				disp('Error: Data not found - type help loaddata for options');
 end
 
