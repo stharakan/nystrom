c3b70e607f9d741b4c4c8f34961156daf7b07540
Sameer Tharakan <sameer@driscoll.ices.utexas.edu>
2015-01-23 09:46:06 -0600

    fixed errors

--- matlab/compute_norms_lambda.m
+++ matlab/compute_norms_lambda.m
@@ -7,6 +7,7 @@ flag = 1; % 0=data_loaded, 1=need to load
 file = 'covtype_libsvm';
 dir = '/org/groups/padas/lula_data/machine_learning/';
 %dir ='/h2/sameer/Documents/research/nystrom/';
+dir = '/work/00921/biros/maverick/data/machine_learning/';
 
 if flag
     clearvars -except dir file sample lambda U L
@@ -22,23 +23,31 @@ sigma_choice = 2;
 sflag = 1; %0=use old sample  , 1=generate new
 lflag = 1; %0=lambda computed , 1=need to compute
 nflag = 1; %0=nystrom computed, 1=generate new
+cvflag = 0; 
 norm_sample_size = 1000;
 runs = 1;
 sample_method = 'random';
 sigma = sigma_given(file,sigma_choice);
 
 %% Select Lambda
+if cvflag
 disp('------Lambda Cross-Val------');
 tic;
 [lambda,spectrum,err] = cv_lambda(X,Y,sigma,nystrom_rank);
-toc;
+toc
 disp(['Lamba chosen: ',num2str(lambda),'; Associated error: ', num2str(err)]);
-%rmatvec = @(rhs) RegNystromMatVec(U,L,lambda,rhs);
+else
+	if ~exist('lambda')
+		lambdastr = 'undefined';
+	else
+		lambdastr = num2str(lambda);
+	disp(['Not running cross-val, previous computed lambda is ', lambdastr]);
+end
+end
 
 %% Full Nystrom decomposition
 disp('-----Nystrom decmp-------')
 [N,d] = size(X);
-%X = single(X);
 nystrom_m = 2*nystrom_rank;
 
 if nflag
@@ -64,46 +73,40 @@ matvec = @(rhs) NystromMatVec(U, L, rhs);
 
 %% Estimate Errors
 disp('-----Estimating kernel approx error------')
-%Relative
-smpidx = randperm(N);
-smpidx = smpidx(1:norm_sample_size);
-w = ones(N,1)./sqrt(N);
-uw = L.*(U'*w);
 
-if(runs ~=1)
-    rel_error = 0;
-    newN = norm_sample_size/runs;
-    for i = 1:runs
-        sidx = smpidx(((i-1)*newN+1):i*newN);
-        estKw = U(sidx,:) *uw;
-        truKw = kernel(X(sidx,:),X,sigma)*w;
-        rel_error = rel_error + sum(abs((estKw - truKw)./truKw))/norm_sample_size;
-    end
-else
-    estKw = U(smpidx,:) * uw;
-    truKw = kernel(X(smpidx,:),X,sigma)*w;
-    rel_error = sum(abs((truKw - estKw)./truKw))/norm_sample_size;
-end
+%Mat-vec error
+tic;
+[abs_err_mv,rel_err_mv] = matvec_errors(X,U,L,norm_sample_size,runs); 
+toc
 
 % Output results
 fprintf('Sigma: %.4f , Rank: %d\n', sigma,nystrom_rank);
-fprintf('Rel error: %.15f\n', rel_error);
-
+fprintf('Rel error: %.15f\n', rel_err_mv);
 
 if ~(isempty(Xtest) || isempty(Ytest))
     disp('-----Estimating test set errors------');    
+   	tic; 
+	[Ntest,~] = size(Xtest);
+	smpidx = randperm(Ntest);
+	smpidx = smpidx(1:norm_sample_size);
     Ktest = kernel(Xtest(smpidx,:),X,sigma);
-    
 
     %Absolute, relative with approx computation
     [Qb,R] = qr(U,0);
-    [Qs,Ls] = eig(R*L*R');
+    [Qs,Ls] = eig(R*diag(L)*R');
     Q = Qb*Qs;
 	clear Qb Qs R
-    w = Q*Ls*Q'*Y;
-    Yguess = Ktest*w;
+    w = Q'*Y;
+    Ls = diag(Ls);
+	reg = sum(Ls>lambda);
+	Ls = Ls(1:reg);
+	lw = w(1:reg)./Ls;
+	w = Q(:,1:reg)*lw;
+	
+	Yguess = Ktest*w;
     absErr_approx = norm(Yguess - Ytest(smpidx))
     relErr_approx = absErr_approx/(norm(Ytest(smpidx)))
+	toc
     
     %Absolute, relative with exact computation
     
