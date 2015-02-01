6f5e734855b5aca888ba314b4c185625cde5202c
Sameer Tharakan <sameer@driscoll.ices.utexas.edu>
2015-02-01 17:34:23 -0600

    sdm.m

--- matlab/sdm.m
+++ matlab/sdm.m
@@ -9,24 +9,20 @@ save_scaled_to_file = ~true;
 whiten_data = ~true;
 
 file = 'covtype_libsvm';
-%file ='ijcnn';
-%file = 'susy';
+cflag = 1;
 [A,Y,Atest,Ytest,~]=loaddata(file,dir);
-%A=randn(1000000,16);  16D dimensionals gamma 1/2/0.45/0.45
-%A = randn(1e6,64); % gamma = 2.62
 
 %%
 %rescale data to [0,1];
 if rescale, A=scale_to_one(A); end
 if whiten_data,  A=whiten(full(A),1e-8); end
 if save_scaled_to_file,csvwrite([file '_scaled.askit'], full(A)); end
-end
 
 %% ==================== parameters
 [n,dim]=size(A);
 k =256; % target rank
 %gamma = 1; H = 1/sqrt(2*gamma);
-H=0.22
+H=0.35
 gamma=1/2/H/H;
 
 opts.eta = 0.1; % decide the precentage of off-diagonal blocks are set to be zero(default 0.1)
@@ -35,27 +31,25 @@ opts.kmeansits=15;
 norm_sample_size = 1000;
 runs = 1;
 
+%% ==================== cross validation
+[lambda,spectrum,err,errors] = cv_lambda_meka(A,Y,gamma,k,opts,cflag);
+disp(['Lambda chosen: ', num2str(lambda),'; Associated error: ', num2str(err)]);
+disp('-----------------------------');
 
-%rsmp = 2048; % sample several rows in K to measure kernel approximation error
-%rsmpind = randsample(1:n,rsmp); 
-%R =sqdist(A(rsmpind,:),A);
-%num_impint  = ceil(sum(sqrt(R(:))<(4*H))/rsmp);
-%tmpK = exp(-(R)*gamma);
-%Kapp = @(x)U(rsmpind',:)*(S*(U'*x));
-%fprintf('Interactions per point that cannot be truncated %d\n',num_impint);
-%%
 %==================== obtain the approximation U and S(K \approx U*S*U^T)
+disp('Computing meka approx');
 t = cputime;
 [U_meka,S_meka] = meka(A,k,gamma,opts); % main function
 display('Done with meka!');
 fprintf('The total time cost for meka is %f secs\n',cputime -t);
-fprintf('***************************\n');
-%==================== measure the relative error
+fprintf('-----------------------------\n');
+
 display('Testing meka!');
 [n,d] = size(A);
 U_meka = full(U_meka);S_meka = full(S_meka);
 
 tic;
+%==================== measure the relative error
 [abs_error, rel_error] = matvec_errors(A,U_meka,S_meka,H,norm_sample_size,runs);
 disp(['The relative approximation error is ', num2str(rel_error)]);
 toc
@@ -63,7 +57,7 @@ toc
 % Test regression
 [U,S] = nystromorth(U_meka,S_meka);
 clear U_meka S_meka
-weights = find_weights(U,S,Y,0);
+weights = find_weights(U,S,Y,lambda);
 
 if ~(isempty(Atest) || isempty(Ytest))
 	disp('Finding regression errors...');
