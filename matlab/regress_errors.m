5359cc8ba240c82f264266a16d3227a78d5f8708
Sameer Tharakan <sameer@driscoll.ices.utexas.edu>
2015-02-05 13:10:51 -0600

    implemented full regression error (approximated), varied input

--- matlab/regress_errors.m
+++ matlab/regress_errors.m
@@ -1,17 +1,22 @@
-function [absErr_approx, relErr_approx,class_err] = regress_errors(X,Xtest,Ytest,w,sigma,varargin)
+function [relErr_approx,class_err] = regress_errors(X,Xtest,Ytest,w,sigma,varargin)
 %REGRESS_ERRORS computes regression errors on the test set Xtest, given a
 %a sigma, weights w, and the correct labels Ytest
-all_flag = 0;
-if length(varargin) < 1
-    norm_sample_size = 1000;
-else
-    [d1,d2] = size(varargin{1});
-    if d1~=d2
-        Um = varargin{2};
-        w = Um * w;
-        all_flag = 1;
-    else
-        norm_sample_size = varargin{1};
+approx = 0;
+ll = length(varargin);
+str = 'exact';
+if ll > 2
+    Um = varargin{1};
+    Un = varargin{2};
+    Ln = varargin{3};
+    w = Um * (Ln.*(Un'*w));
+    all_flag = 0;
+    approx = 1;
+    str = 'approx';
+    
+    if ll == 4
+        all_flag = varargin{4};
+    elseif ll >4  
+        disp('Incorrect format for regresserrors.m');
     end
 end
 
@@ -20,12 +25,17 @@ relErr_approx = 0;
 class_err = 0;
 [N,d] = size(X);
 [Ntest,~] = size(Xtest);
+norm_sample_size = 1000;
 
-if ~all_flag
-    smpidx = floor(1:Ntest/norm_sample_size:Ntest);
-else
-    disp('Testing entire set with Nystrom approx.');
-    smpidx = 1:Ntest;
+smpidx = floor(1:(Ntest/norm_sample_size):Ntest);
+
+if approx
+    if all_flag
+        smpidx = 1:Ntest;
+    else
+        smpidx = floor(1:(Ntest/norm_sample_size):Ntest);
+    end
+
     if (N * Ntest > 10E9)
         disp('Make sure SAMPLE X is entered, matrices too large. Exiting..');
         return;
@@ -37,7 +47,7 @@ Ktest = kernel(Xtest(smpidx,:),X,sigma);
 Yguess = Ktest*w;
 absErr_approx = norm(Yguess - Ytest(smpidx));
 relErr_approx = absErr_approx/(norm(Ytest(smpidx)));
-
 class_err = sum( sign(Ytest(smpidx)) == sign(Yguess) )/length(smpidx);
+disp(['Error tested on ', num2str(length(smpidx)),' points', ...
+    ' with the ', str, ' kernel']);
 end
-
