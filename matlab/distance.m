e173399571a76325fccc2ebf8a307d4a75ba5264
Sameer Tharakan <sameer@driscoll.ices.utexas.edu>
2015-02-05 13:13:22 -0600

    commented out try catch

--- matlab/distance.m
+++ matlab/distance.m
@@ -1,4 +1,4 @@
-function dist=distance(X,x);
+function dist=distance(X,x) 
 % dist=distance(X,x)
 %
 % computes the pairwise L2 (not squared) distance matrix between any column
@@ -17,40 +17,40 @@ function dist=distance(X,x);
 % Dist=distance(X);
 %
 [D,N] = size(X);
-     
-try 
- if(nargin>=2)
+
+%try
+if(nargin>=2)
     % PAIRWISE DISTANCES
-  [D,n] = size(x);
-  X2 = sum(X.^2,1);
-  x2 = sum(x.^2,1);
-  dist = repmat(x2,N,1);
-	dist = dist + repmat(X2',1,n);
-	dist = dist - 2*X'*x;
- else    
-  [D,N] = size(X);
-  if(exist('addv') & exist('addh'))
-   X2 = sum(X.^2,1);
-   dist=addh(addv(-2*X'*X,X2),X2);
-  else
-   X2 = repmat(sum(X.^2,1),N,1);
-   dist = X2+X2'-2*X'*X;   
-%   fprintf('Please install addv and addh.\n');
-  end;
- end;
- 
-catch
-  assert(false);
-  dist=zeros(N);
-  tic;
-  fprintf('Not enough Memory\nComputing distance line by line ...\n');
-  for i=1:N-1
-    j=(i+1):N;
-    dist(i,j)= ones(1,length(j)).*sum(X(:,i).^2)+sum(X(:,j).^2)-2.*(X(:,j)'*X(:,i))';
-    dist(j,i)=dist(i,j)';    
-  end;
-  fprintf('\n');
+    [D,n] = size(x);
+    X2 = sum(X.^2,1);
+    x2 = sum(x.^2,1);
+    dist = repmat(x2,N,1);
+    dist = dist + repmat(X2',1,n);
+    dist = dist - 2*X'*x;
+else
+    [D,N] = size(X);
+    if(exist('addv') & exist('addh'))
+        X2 = sum(X.^2,1);
+        dist=addh(addv(-2*X'*X,X2),X2);
+    else
+        X2 = repmat(sum(X.^2,1),N,1);
+        dist = X2+X2'-2*X'*X;
+        %fprintf('Please install addv and addh.\n');
+    end;
 end;
+
+%catch
+%  assert(false);
+%  dist=zeros(N);
+%  tic;
+%  fprintf('Not enough Memory\nComputing distance line by line ...\n');
+%  for i=1:N-1
+%    j=(i+1):N;
+%    dist(i,j)= ones(1,length(j)).*sum(X(:,i).^2)+sum(X(:,j).^2)-2.*(X(:,j)'*X(:,i))';
+%    dist(j,i)=dist(i,j)';
+%  end;
+%  fprintf('\n');
+%end;
 
 dist=sqrt(dist);
 
